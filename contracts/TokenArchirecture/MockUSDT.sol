67f3d3e14ff37fc5a6c6355fc9692c15d9866278
DannyLurker <dannycode992@gmail.com>
2025-07-31 15:22:40 +0700

    chore: initialize project with Hardhat and OpenZeppelin contracts
    - Added package.json with devDependencies for Hardhat and OpenZeppelin
    - Created Lock.ts test file with comprehensive tests for the Lock contract
    - Added tsconfig.json for TypeScript configuration

---
+++ contracts/TokenArchirecture/MockUSDT.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.28;
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {AccessControl} from "@openzeppelin/contracts/access/AccessControl.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Pausable.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
+
+contract TokenArchitecture is ERC20, ERC20Pausable, AccessControl, ERC20Permit {
+    //Deklarasi ERC20: Membuat token dengan nama "MockUSDT" dan inisial "MSD"
+
+    //Deklarasi ERC20Permit: Tujuan dari ERC20Permit("Danny Token") di constructor adalah untuk menyetel nama token ke dalam DOMAIN_SEPARATOR, agar tanda tangan permit() hanya valid untuk kontrak dan chain tertentu, mencegah penyalahgunaan (replay attack), memastikan tanda tangan dapat diverifikasi dengan benar, serta dengan permit kita bisa mengizinkan orang lain memakai token kita tanpa harus menggunakan approve lalu transferform, jadinya hanya 1 transaksi saja
+
+    constructor() ERC20("MockUSDT", "MSD") ERC20Permit("MockUSDT") {
+        uint256 totalSupply = 100_000_000 * 10 ** decimals();
+        _mint(address(this), totalSupply);
+    }
+
+    // The following functions are overrides required by Solidity.
+    function _update(
+        address from,
+        address to,
+        uint256 value
+    ) internal override(ERC20, ERC20Pausable) {
+        super._update(from, to, value);
+    }
+}
