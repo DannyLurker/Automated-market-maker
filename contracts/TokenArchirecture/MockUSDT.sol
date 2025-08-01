f01666ef5033ee972ade26036d78a7dbb6f56dad
DannyLurker <dannycode992@gmail.com>
2025-08-01 19:17:20 +0700

    feat: implement initial owner and minter role in TokenArchitecture contract

--- contracts/TokenArchirecture/MockUSDT.sol
+++ contracts/TokenArchirecture/MockUSDT.sol
@@ -4,15 +4,37 @@ import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import {AccessControl} from "@openzeppelin/contracts/access/AccessControl.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Pausable.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
-contract TokenArchitecture is ERC20, ERC20Pausable, AccessControl, ERC20Permit {
+contract TokenArchitecture is
+    ERC20,
+    ERC20Pausable,
+    AccessControl,
+    ERC20Permit,
+    Ownable
+{
     //Deklarasi ERC20: Membuat token dengan nama "MockUSDT" dan inisial "MSD"
 
     //Deklarasi ERC20Permit: Tujuan dari ERC20Permit("Danny Token") di constructor adalah untuk menyetel nama token ke dalam DOMAIN_SEPARATOR, agar tanda tangan permit() hanya valid untuk kontrak dan chain tertentu, mencegah penyalahgunaan (replay attack), memastikan tanda tangan dapat diverifikasi dengan benar, serta dengan permit kita bisa mengizinkan orang lain memakai token kita tanpa harus menggunakan approve lalu transferform, jadinya hanya 1 transaksi saja
 
-    constructor() ERC20("MockUSDT", "MSD") ERC20Permit("MockUSDT") {
+    // Untuk awal awal kita bisa gunakan sistem intialOwner dan ketika contract sudah stabil baru kita renounce atau pun serahkan ke contract sepenuhnya
+
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+
+    constructor(
+        address initialOwner
+    ) ERC20("MockUSDT", "MSD") ERC20Permit("MockUSDT") Ownable(initialOwner) {
         uint256 totalSupply = 100_000_000 * 10 ** decimals();
         _mint(address(this), totalSupply);
+        _grantRole(MINTER_ROLE, initialOwner);
+    }
+
+    function pause() external onlyOwner {
+        _pause();
+    }
+
+    function unpause() external onlyOwner {
+        _unpause();
     }
 
     // The following functions are overrides required by Solidity.
