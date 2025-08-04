84190bcd22933ea5bb9e04b3829a03f46242b11d
DannyLurker <dannycode992@gmail.com>
2025-08-04 20:21:12 +0700

    feat: implement DexPair contract with initialization and liquidity management functions

--- contracts/core/DexFactory.sol
+++ contracts/core/DexFactory.sol
@@ -7,16 +7,35 @@ contract DexFactory {
     mapping(address => mapping(address => address)) public getPair;
     address[] public allPairs;
 
+    event CreatePair(address tokenA, address tokenB, address pair);
+
     function createPair(
-        address tokenA,
-        address tokenB
+        address _tokenA,
+        address _tokenB
     ) external returns (address pair) {
+        require(_tokenA != _tokenB, "Identical tokens not allowed");
+        require(
+            _tokenA != address(0) || _tokenB != address(0),
+            "Zero address not allowed"
+        );
+
+        // Kalau pair nya itu ada yang zero address berarti masih belum ada, tapi kalau udah ada, dia tidak akan mengembalikan zero address yang dimana 0xff123.. == 0x0000: false, code akan langsung berhenti
+
+        // Kalau 0x000.. == 0x000...: true, dia bakal lanjut eksekusi code yang di bawah
+
+        require(getPair[_tokenA][_tokenB] == address(0), "Pair already exists");
+
         // Buat 1 pair contract baru
+        //NOTE: Tidak perlu melakukan deklarasi type lagi dikarena sudah di tentukan name variablenya returns
         pair = address(new DexPair());
 
+        //Initialize pair
+        DexPair(pair).initialize(_tokenA, _tokenB);
+
         // Simpan ke mapping
-        getPair[tokenA][tokenB] = pair;
-        getPair[tokenB][tokenA] = pair;
+        getPair[_tokenA][_tokenB] = pair;
+        getPair[_tokenB][_tokenA] = pair;
+        emit CreatePair(_tokenA, _tokenB, pair);
 
         // Simpan ke array
         allPairs.push(pair);
--- contracts/core/DexPair.sol
+++ contracts/core/DexPair.sol
@@ -1,4 +1,22 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.28;
 
-contract DexPair {}
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
+
+contract DexPair is ERC20, ReentrancyGuard {
+    address public factory;
+    address public tokenA;
+    address public tokenB;
+    uint256 public constant MINIMUM_LIQUIDITY = 1000;
+
+    constructor() ERC20("DexPair LP", "DLP") {
+        factory = msg.sender;
+    }
+
+    function initialize(address _tokenA, address _tokenB) external {
+        require(msg.sender == factory, "DexPair: Forbidden");
+        tokenA = _tokenA;
+        tokenB = _tokenB;
+    }
+}
